df7ed218b1c73b3e2254377d0ac438d8d89550af
Rubén Casillas <rubencasillas00@hotmail.com>
2016-05-31 21:33:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/*Se ha creado el siguiente algoritmo que busca que el usuario ingrese elementos hasta que indique lo contrario, para lo cual
+ingresará la palabra "fin". Este algoritmo no está completo, se pide arreglarlo.
+
+PREGUNTA 1
+
+*/
+
+
+var suma = 0;
+do
+var ingreso = alert("ingresa un número");
+while (ingreso != "fin");
+console.log(suma)
